c653d5c445f8182fe790b33690914d189ba8dbaa
emersonoliveirah <emersonoliveirah@gmail.com>
2025-01-07 19:45:41 -0300

    Create UserService.

---
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,30 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Merchant cannot make transactions");
+        }
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Insufficient balance");
+        }
+    }
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("User not found"));
+    }
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
+}
