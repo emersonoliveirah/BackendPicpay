073158ad10c346e4055bf3b490608a980c5b1d80
emersonoliveirah <emersonoliveirah@gmail.com>
2025-01-08 00:11:53 -0300

    Modified UserController

--- src/main/java/com/picpaysimplificado/controllers/UserController.java
+++ src/main/java/com/picpaysimplificado/controllers/UserController.java
@@ -8,11 +8,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("/users")
+@RestController()
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
