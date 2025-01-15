ba1da71c5d96d1a33b07f1409f89191ff8830ca5
emersonoliveirah <emersonoliveirah@gmail.com>
2025-01-15 00:25:00 -0300

    Lombok Getters and Setters.

--- src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
+++ src/main/java/com/picpaysimplificado/domain/transaction/Transaction.java
@@ -27,44 +27,4 @@ public class Transaction {
     private User receiver;
     private LocalDateTime timestamp;
 
-
-    public User getReceiver() {
-        return receiver;
-    }
-
-    public void setReceiver(User receiver) {
-        this.receiver = receiver;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public User getSender() {
-        return sender;
-    }
-
-    public void setSender(User sender) {
-        this.sender = sender;
-    }
-
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
 }
